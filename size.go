c6199bc64f69359a6dae8bff237dc7c701df1a6e
Ashk <ashkan.khd.q@gmail.com>
2020-09-11 17:22:42 +0430

    refactor

--- size.go
+++ size.go
@@ -101,9 +101,7 @@ func AbortIfTooLarge(key, valType string) gin.HandlerFunc {
 		case "Text":
 			context.PostForm(key)
 		case "File":
-			if _, err := context.FormFile(key); err != nil {
-				panic(err)
-			}
+			context.FormFile(key)
 		}
 		if len(context.Errors) > 0 {
 			fmt.Println("Sent Data Was Too Large")
